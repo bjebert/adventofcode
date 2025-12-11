86d695f06df828b0008ee856ea35e7afb5b8d0cd
Blake Ebert <blake.ebert@outlook.com>
2025-12-11 14:31:09 +1000

    day 9

--- 2025/09.R
+++ 2025/09.R
@@ -24,7 +24,6 @@ inp <- inr
 
 n <- t(sapply(strsplit(inp, ","), as.numeric))
 
-
 e <- sapply(1:(nrow(n)-1), function(i) {
     sapply((i+1):nrow(n), function(j) {
         Reduce(`*`, abs(n[i,] - n[j,]) + 1)
@@ -35,49 +34,165 @@ max(sapply(e, max))
 
 # Part 2 ------------------------------------------------------------------
 
-inp <- inr
-inp <- inx
-
-# whichever way the inputs come in - the rectangle has to "look" that way
-n <- t(sapply(strsplit(inp, ","), as.numeric))
-
-is_safe <- function(i, j, best = 0) {
-    a <- n[i,]
-    b <- n[j,]
+is_valid <- function(i, j) {
+    if(j == i + 1 || (i == 1 && j == N)) return(TRUE)
     
-    if(j == i + 1 || (i == 1 && j == nrow(n))) return(TRUE)
-    if(any(a - b == 0)) return(TRUE)
+    rect_x <- sort(n[c(i,j), 1])
+    rect_y <- sort(n[c(i,j), 2])
     
-    rng_x <- a[1]:b[1]
-    rng_y <- a[2]:b[2]
-    size <- as.double(length(rng_x)) * length(rng_y)
+    # Along each edge of the rectangle, find the range that isn't part of an existing wall
     
-    if(size < best) return(FALSE)  # don't bother..
+    # Vertical walls: ---
+    # rect_x[1], rect_y[1]:rect_y[2]
+    vy1 <- sort(n[n[, 1] == rect_x[1], ][, 2])
     
-    # for each rng_y, look right
+    # rect_x[2], rect_y[1]:rect_y[2]
+    vy2 <- sort(n[n[, 1] == rect_x[2], ][, 2])
     
-    for(y in rng_y) {
-        rng_x        
+    # Horizontal walls: ---
+    # rect_x[1]:rect_x[2], rect_y[1]
+    hx1 <- sort(n[n[, 2] == rect_y[1], ][, 1])
+    
+    # rect_x[1]:rect_x[2], rect_y[2]
+    hx2 <- sort(n[n[, 2] == rect_y[2], ][, 1])
+    
+    get_search_segment <- function(rect_b, wall) {
+        #' One of the bounds of each of these walls always connects to our rectangle
         
+        if(identical(rect_b, wall)) {
+            # Wall goes the whole way along rectangle edge, so no search needed (it's valid.)
+            return(NULL)
+        }
         
+        if(rect_b[1] == wall[1]) {
+            if(wall[2] > rect_b[2]) {
+                return(NULL)  # Wall fully contains edge
+            }
+            
+            return(c(wall[2] + 0.5, rect_b[2] - 0.5))
+        } else if(rect_b[2] == wall[2]) {
+            if(wall[1] < rect_b[1]) {
+                return(NULL)  # Wall fully contains edge
+            }
+            
+            return(c(rect_b[1] + 0.5, wall[1] - 0.5))
+        } else {
+            # No overlap between rectangle bounds and wall
+            return(c(rect_b[1] + 0.5, rect_b[2] - 0.5))
+        }
     }
     
+    search_y1 <- get_search_segment(rect_y, vy1)
+    search_y2 <- get_search_segment(rect_y, vy2)
+    search_x1 <- get_search_segment(rect_x, hx1)
+    search_x2 <- get_search_segment(rect_x, hx2)
+    
+    cast_bounds <- function(search, fixed, y = TRUE) {
+        if(is.null(search)) {
+            return(TRUE)
+        }
+        
+        d <- if(y) 1 else 2
+        candidates <- if(y) edges[["vert"]] else edges[["horz"]]
+        
+        # Only look for candidates in a certain direction (right for y=TRUE, down for y=FALSE)
+        candcast <- candidates[sapply(candidates, function(x) x[[1]][d]) > fixed]
+        bounds <- lapply(candcast, function(cand) sapply(cand, function(x) x[3-d]))        
+        
+        if(length(bounds) == 0) {
+            return(FALSE)
+        }
+        
+        bounds <- bounds[order(sapply(bounds, function(x) x[1]))]
+        
+        # Remove bounds/walls that are entirely outside our search range.
+        bounds <- bounds[sapply(bounds, function(x) x[1] <= search[2] & x[2] >= search[1])]
+        
+        if(length(bounds) == 0) {
+            return(FALSE)
+        }
+        
+        # We need every number inside search to hit bounds an odd number of times
+        if(length(bounds) == 1) {
+            bounds_odd <- TRUE
+        } else {
+            # See if this hack works?
+            bounds_table <- table(unlist(bounds))
+            
+            middle_good <- length(bounds_table) <= 2 ||
+                all(bounds_table[2:(length(bounds_table)-1)] == 2)
+            
+            bounds_odd <- head(bounds_table, 1) == 1 &&
+                tail(bounds_table, 1) == 1 && middle_good
+        }
+        
+        bounds_contain_search <- bounds[[1]][1] <= search[1] && bounds[[length(bounds)]][2] >= search[2]
+        
+        return(bounds_odd && bounds_contain_search)
+    }
+    
+    cast_bounds(search_y1, rect_x[1], y = TRUE) && 
+        cast_bounds(search_y2, rect_x[2], y = TRUE) &&
+        cast_bounds(search_x1, rect_y[1], y = FALSE) && 
+        cast_bounds(search_x2, rect_y[2], y = FALSE)
 }
 
-is_safe(1, 3)  # false
-i <- 1
-j <- 3
+
+get_edges <- function(n) {
+    edges <- list(horz = NULL, vert = NULL)
+    
+    for(i in 1:N) {
+        j <- if(i == N) 1 else i+1
+        edge <- list(n[i, ], n[j, ])
+        
+        if(n[i, 1] == n[j, 1]) {
+            edges[["vert"]][[length(edges[["vert"]]) + 1]] <- edge[order(sapply(edge, function(x) x[2]))]
+        } else if(n[i, 2] == n[j, 2]) {
+            edges[["horz"]][[length(edges[["horz"]]) + 1]] <- edge[order(sapply(edge, function(x) x[1]))]
+        } else {
+            stop()
+        }
+    }
+    
+    edges[["horz"]] <- edges[["horz"]][order(sapply(edges[["horz"]], function(x) x[[1]][2]), sapply(edges[["horz"]], function(x) x[[1]][1]))]
+    edges[["vert"]] <- edges[["vert"]][order(sapply(edges[["vert"]], function(x) x[[1]][1]), sapply(edges[["vert"]], function(x) x[[1]][2]))]
+    
+    return(edges)
+}
 
 
+inp <- inr
+n <- t(sapply(strsplit(inp, ","), as.numeric))
+N <- nrow(n)
+edges <- get_edges(n)
 best <- 0
-for(i in 1:(nrow(n) - 1)) {
-    for(j in (i+1):nrow(n)) {
-        if(is_safe(i, j, best)) {
-            size <- Reduce(`*`, abs(n[i,] - n[j,]) + 1)
-            if(size > best) {
-                print(size)
-                best <- size
-            }
+
+
+for(i in 1:(N-1)) {
+    for(j in (i+1):N) {
+        size <- Reduce(`*`, abs(n[i,] - n[j,]) + 1)
+        if(size <= best) {
+            next
+        }
+        
+        if(is_valid(i, j)) {
+            print(c(i, j))
+            best <- size
+            print(sprintf("BEST: %s", best))
         }
     }
 }
+
+
+# 144547308 (43,203): valid, but I think we can do bigger
+# 272424405: wrong: outside box.
+
+i <- 219
+j <- 249
+p <- n[c(i,j), ]
+
+plot(n[,1], n[,2], type = "l", xaxt = "n", yaxt = "n")
+axis(side = 1, at = p[, 1])
+axis(side = 2, at = p[, 2])
+abline(h = p[,2], lty = 2, col = 'blue')
+abline(v = p[,1], lty = 2, col = 'blue')
